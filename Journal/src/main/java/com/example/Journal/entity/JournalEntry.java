d373771229331d45c146c2a6b5307afc618641d0
harshad diwate <harshaddiwate85@gmail.com>
2026-02-24 23:12:11 +0530

    feat : add lombok library for run time boilerplate code generation

--- Journal/src/main/java/com/example/Journal/entity/JournalEntry.java
+++ Journal/src/main/java/com/example/Journal/entity/JournalEntry.java
@@ -1,5 +1,8 @@
 package com.example.Journal.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +11,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document
+@Data
 public class JournalEntry {
     @Id
     private ObjectId id;
@@ -15,35 +19,4 @@ public class JournalEntry {
     private String Title;
     private LocalDateTime date;
 
-
-    public String getTitle() {
-        return Title;
-    }
-
-    public void setTitle(String title) {
-        Title = title;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public String getContent(){
-        return content;
-    }
-
-    public void setContent(String content){
-        this.content = content;
-    }
-
-    public void setId(ObjectId id){
-        this.id=id;
-    }
-    public ObjectId getId(){
-        return id;
-    }
 }
