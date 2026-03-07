8117f3bd23b151245c002be11d9cd1bcb0dc0bf3
harshad diwate <harshaddiwate85@gmail.com>
2026-03-08 00:58:29 +0530

    refactor: enhance user saving with error logging and exception handling.

--- Journal/src/main/java/com/example/Journal/service/UserService.java
+++ Journal/src/main/java/com/example/Journal/service/UserService.java
@@ -4,6 +4,8 @@ import com.example.Journal.Repository.JournalEntryRepository;
 import com.example.Journal.Repository.UserRepository;
 import com.example.Journal.entity.User;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -19,11 +21,19 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     // Use for new user registration only - encodes password
-    public void save(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        userRepository.save(user);
+    public void save(User user) throws Exception {
+        try{
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            userRepository.save(user);
+        }
+      catch(Exception e){
+            logger.error("Error occured for user {}",user.getUsername(),e); // {} are placeholders we can insert data into it here the username is inserted in the log along with the error
+          throw new Exception(e);
+      }
     }
 
     // Use for updating existing users - does NOT re-encode password
