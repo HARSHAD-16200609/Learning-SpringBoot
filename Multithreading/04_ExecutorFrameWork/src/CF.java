0182817db1cac801382642f4dfebc8d6775fef3f
harshad diwate <harshaddiwate85@gmail.com>
2026-04-06 23:58:09 +0530

    feat: update CompletableFuture examples to use fixed thread pool and improve timeout handling

--- Multithreading/04_ExecutorFrameWork/src/CF.java
+++ Multithreading/04_ExecutorFrameWork/src/CF.java
@@ -5,7 +5,7 @@ public class CF {
     public static void main(String [] args) throws InterruptedException, ExecutionException {
 
 
-
+ExecutorService executor = Executors.newFixedThreadPool(2);
         CompletableFuture<String> f1 = CompletableFuture.supplyAsync(()->{
             try {
                 Thread.sleep(5000);
@@ -13,7 +13,7 @@ public class CF {
                Thread.currentThread().interrupt();
             }
             return "This is thread " +Thread.currentThread().getName();
-        }).orTimeout(1000, TimeUnit.SECONDS).exceptionally(x -> "ERROR OCCURED : " + x);
+        },executor).orTimeout(6000, TimeUnit.MILLISECONDS).exceptionally(x -> "ERROR OCCURED : " + x);
 
         CompletableFuture<String> f2 = CompletableFuture.supplyAsync(()->{
             try {
@@ -22,10 +22,15 @@ public class CF {
                Thread.currentThread().interrupt();
             }
             return "This is thread " +Thread.currentThread().getName();
-        }).orTimeout(1000, TimeUnit.SECONDS).exceptionally(x -> "ERROR OCCURED : " + x);
+        },executor).orTimeout(6000, TimeUnit.MILLISECONDS).exceptionally(x -> "ERROR OCCURED : " + x);
 
-        System.out.println(f1.get());
-        System.out.println(f2.get());
+        CompletableFuture.allOf(f1, f2)
+                .thenRun(() -> {
+                    System.out.println(f1.join());
+                    System.out.println(f2.join());
+
+                })
+                .join();
         System.out.println("Main Thread");
 
     }
